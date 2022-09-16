ad7edd063a496c427775658903a8f9081ca7e56c
Agnesjude <agnessahayarani@gmail.com>
2022-09-16 16:59:38 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import DonutChart from 'react-donut-chart';
+import donut_data from './components/donut_data.json'
+import table_data from './components/table_data.json'
 
 function App() {
+  
+  const [value,setValue] = useState("");
+  const [result,setResult] = useState([{
+    case_status : "", 
+    case_id : 0
+  }]);
+
+  useEffect(() => {
+    if(value=="")
+    setResult([...table_data]);
+    else{
+      const filtered = table_data.filter(case_status => {
+        return case_status.case_status === value;
+      });
+      setResult([...filtered]);
+    }
+  },[value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <DonutChart
+        data={donut_data}
+        onClick={(item, toggled) =>          
+          toggled
+            ? setValue(item.label)
+            : setValue("")           
+        }
+      />
+      <table>
+        <tr>
+          <th>
+            Case Status
+          </th>
+          <th>
+            Case Id
+          </th>
+        </tr>
+        {result.map(filteredName => (
+          <>
+            <tr>
+              <td>
+              {filteredName.case_status}
+              </td>
+              <td>
+              {filteredName.case_id}
+              </td>
+            </tr>
+          </>
+          
+        ))
+        }
+      </table>
+        
     </div>
   );
 }
